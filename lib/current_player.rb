da7629e5033f33f87ed99b89e25e2c622cdf26b7
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-01 23:58:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if space == "X" || "O"
+    if space == "X"
       counter+=1
+    elsif space == "O"
     end
   end
   return counter
