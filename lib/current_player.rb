68fa2808b89c676cc75406988daf2f7edc36b345
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-02 00:16:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -19,3 +19,12 @@ def turn_count(board)
   end
   return counter
 end
+
+def current_player(board)
+  turn_count = turn_count(board)
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
